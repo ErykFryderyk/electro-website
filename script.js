42b1f198ef86b58141b2b47bada2f217793f706d
Grzegorz Kostrzewa <grzesiek@o2.pl>
2019-10-09 23:51:12 +0200

    add new files

---
+++ script.js
@@ -0,0 +1,81 @@
+
+//slider
+let sliderImages = document.querySelectorAll('.slide'),
+arrowLeft = document.querySelector('#arrow-left'), 
+arrowRight = document.querySelector('#arrow-right'),
+current=0;
+
+//images all clear
+function reset(){
+    for(let i =0; i <sliderImages.length; i++){
+        sliderImages[i].style.display = 'none';
+    }
+}
+
+// Init slider
+function startSlide(){
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+
+//show previous
+function slideLeft(){
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+// show next
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+
+//click arrow left
+arrowLeft.addEventListener('click', function(){
+    if(current ===0){
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+
+//click arrow right
+arrowRight.addEventListener('click', function (){
+    if (current === sliderImages.length - 1) {
+        current = - 1;
+    }
+    slideRight();
+});
+
+startSlide();
+
+
+//hamburger-button and menu-small-screen 
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navigation');
+
+
+function handleClick() {
+    hamburger.classList.toggle('hamburger--active');
+    navMenu.classList.toggle('navigation--active');
+}
+
+hamburger.addEventListener('click', handleClick);
+
+
+// scrollToTop
+let myButton = document.querySelector('#top');
+
+window.onscroll = function(){
+    'use strict';
+    if(window.pageXOffset >400) {
+        muButton.style.display = 'none';
+    }
+}
+
+myButton.onclick = function(){
+    'use strict';
+    window.scrollTo(0,0);
+
+};
